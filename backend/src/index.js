6a6fa50b97dee05706fd07c6b584d6e859cb8ed2
vtorres96 <victorcdc96@gmail.com>
2019-06-17 19:19:18 -0300

    creating the apllication index

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb+srv://vtorres96:03v01t96m@cluster0-bfkrt.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+server.listen(3333);

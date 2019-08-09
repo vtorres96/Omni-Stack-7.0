06fe96037762d741db8ac7c904f9dab3a3309765
vtorres96 <victorcdc96@gmail.com>
2019-08-08 21:21:04 -0300

    optimizing server adding dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,21 @@ const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+require('dotenv/config');
+
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://username:<password>@cluster0-bfkrt.mongodb.net/test?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-bfkrt.mongodb.net/${
+    process.env.DB_NAME
+  }?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+  },
+);
 
 app.use((req, res, next) => {
   req.io = io;
@@ -24,4 +31,7 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resiz
 
 app.use(require('./routes'));
 
-server.listen(3333);
+server.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Servidor rodando na porta: 3333');
+});
